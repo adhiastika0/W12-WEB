e1351a2709e96836b19375c5bf54557f4e1981d4
Justicly <adhiastika19@gmail.com>
2023-11-12 13:07:28 +0800

    Add change name form

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,64 @@
-"use client"
+'use client';
 
-import Image from 'next/image'
-import styles from './page.module.css'
-import foto from '/public/assets/profil.png'
-import { useState } from 'react'
+import Image from 'next/image';
+import styles from './page.module.css';
+import foto from '/public/assets/profil.png';
+import { useRef, useState } from 'react';
 
 export default function Home() {
-const [bravo, setBravo] = useState(0)
+  const [bravo, setBravo] = useState(0);
 
-function handlerTambah(){
-  setBravo(bravo+1)
-}
+  function handlerTambah() {
+    setBravo(bravo + 1);
+  }
 
+  const input = useRef(null);
+
+  const [nama, setNama] = useState('Gusti Ngurah Adhi Astika');
+  function handlerNama() {
+    setNama(input.current.value);
+    input.current.value = '';
+  }
 
   return (
-    <div className='body'>
-      <div className='banner-container'>
-        <div className='header-banner-wrapper'>
-          <div className='profile-header-banner'>
+    <div className="body">
+      <div className="banner-container">
+        <div className="header-banner-wrapper">
+          <div className="profile-header-banner">
             <Image
               src={foto}
               width={120}
               height={120}
-              alt="Picture of the author">
-            </Image>
+              alt="Picture of the author"
+            ></Image>
           </div>
-          <div className='content-header-banner'>
-            <h1>Gusti Ngurah Adhi Astika</h1>
-            <div className='bio-nim-header-banner'>
+          <div className="content-header-banner">
+            <h1>{nama}</h1>
+            <div className="bio-nim-header-banner">
               <p>D121211029</p>
               <p>“To Infinity and Beyond”</p>
             </div>
           </div>
         </div>
         <div className="cta-banner-wrapper">
+          <input
+            className="cta-input"
+            ref={input}
+            type="text"
+            placeholder="Masukkan Nama"
+          />
           {/* Tombol CTA */}
-            <div className='cta-button' onClick={handlerTambah}>
-              <p>Halo! {bravo}</p>
-            </div>
+          <div
+            className="cta-button"
+            onClick={() => {
+              handlerTambah();
+              handlerNama();
+            }}
+          >
+            <p>Ganti Nama! {bravo}x</p>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
